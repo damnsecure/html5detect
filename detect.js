9800c6b1037a7d90a9960a2557a8fa90ce0a6397
damnsecure <ruben@damnsecure.org>
2013-05-25 09:42:52 +1000

    Added functionatily to read message that are being send

--- detect.js
+++ detect.js
@@ -1,9 +1,20 @@
 //Detect if onmessage is used if so notify background.html
 //Thanks to chromesniffer for providing a plugin that made me understand how this all works.
 (function () {
-	window.onmessage = function(message){
-		console.log(message);
+	var postMessageORIG = window.postMessage;
+	window.postMessage = function(message, destination){
+		console.log("Sending: ", message, destination);
+		postMessageORIG(message, destination);
+		notify();
+	}
 
+	window.onmessage = function(message){
+		console.log("Received: ", message);
+		notify();
+	}
+
+	function notify()
+	{
 		var jsonString = JSON.stringify({"html5onmessage_detected": true});
 		var meta = document.getElementById('html5detect_meta');
 		meta.content = jsonString;
